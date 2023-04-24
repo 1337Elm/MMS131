c0b647f82a1d7650aef2a133500f4da6aab39ddb
Benjamin <benjamin.elmjonsson@gmail.com>
2023-04-24 02:23:44 +0200

    only distance calc left

--- Assignment2/2.4/Task24.py
+++ Assignment2/2.4/Task24.py
@@ -8,6 +8,7 @@
 import numpy as np
 import matplotlib
 import warnings
+import sys
 
 #---------------------------
 #Ignore deprecation warnings
@@ -37,20 +38,53 @@ def read_file(filename: str) -> np.array:
 
 
 def find_path(matrix: np.array) ->list:
-    path = []
     startNode = np.where(matrix == 2)
     endNode = np.where(matrix == 3)
+    current_node = startNode
+    
+    distance = []
+    path = []
+    path.append(startNode)
 
-    neighbors = find_neighbors(matrix,startNode)
+    while current_node != endNode:
+        neighbors = find_neighbors(matrix,current_node,case = 1)
 
+        for i in range(len(neighbors)):
+            distance.append(calc_distance(matrix,neighbors[i]))
+        
+        matrix[current_node[0],current_node[1]] = 1 #1 means cant visit,
+        #which is true for already visited nodes, i.e this node has now been visited
+
+        current_node = neighbors[np.where(distance == np.min(distance))] #Update current node to node whish has min distance to target
+        path.append(current_node)
+
+    path.append(endNode)
     return path
 
 
+def find_neighbors(matrix: np.array, node: list,case: int) -> list:
+    neighbors = []
+    i = int(node[0])
+    j = int(node[1])
+
+    if case == 1:
+        combinations = [[i+1,j],[i-1,j],[i,j+1],[i,j-1]]
+    else:
+        combinations = [[i+1,j],[i-1,j],[i,j+1],[i,j-1],[i+1,j+1],[i-1,j-1],[i+1,j-1],[i-1,j+1]]
+
+    for i in combinations:
+        try:
+            val = matrix[i[0],i[1]]
+            
+            if val == 0 or val == 3:
+                neighbors.append([i[0],i[1]])
+        except:
+            pass
+    
+    return neighbors
+
+
 def calc_distance(matrix: np.array, currentNode: list) -> int:
-    pass
-
-
-def find_neighbors(matrix: np.array, node: list) -> list:
     pass
 
 
@@ -58,9 +92,8 @@ def main():
     filename = "maze_small.txt"
 
     matrix = read_file("Assignment2/2.4/" + filename)
-    print(matrix)
-    find_path(matrix)
-
+    
+    print(find_path(matrix))
 
 
 if __name__ == '__main__':
